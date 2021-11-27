0f1d0d0dd38e01c62fd4338e9c3aa48bec124631
Dima <yadima2001@gmail.com>
2021-11-27 18:38:43 +0200

    comparer

---
+++ Module2HW6/Helpers/ApplianceComparer.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using Module2HW6.Models;
+
+namespace Module2HW6.Helpers
+{
+    public class ApplianceComparer : IComparer
+    {
+        public int Compare(object x, object y)
+        {
+            if (x == null || y == null)
+            {
+                return 0;
+            }
+
+            var xAppliance = x as Appliance;
+            var yAppliance = y as Appliance;
+
+            switch (xAppliance?.PowerConsumption - yAppliance?.PowerConsumption)
+            {
+                case > 0:
+                    return 1;
+                case < 0:
+                    return -1;
+            }
+
+            return 0;
+        }
+    }
+}
