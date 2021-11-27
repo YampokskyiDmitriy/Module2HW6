c56346b4734ebae18166873f41e2d90e8a5a6d63
Dima <yadima2001@gmail.com>
2021-11-27 18:38:59 +0200

    array extension

---
+++ Module2HW6/Helpers/ArrayExtension.cs
@@ -0,0 +1,22 @@
+﻿using Module2HW6.Models;
+
+namespace Module2HW6.Helpers
+{
+    public static class ArrayExtension
+    {
+        public static Appliance FindByPower(this Appliance[] appliances, int maxPower)
+        {
+            Appliance result = null;
+            foreach (var appliance in appliances)
+            {
+                if (appliance.PowerConsumption == maxPower)
+                {
+                    result = appliance;
+                    break;
+                }
+            }
+
+            return result;
+        }
+    }
+}
