16d1283c84f7cc10e804b19112a1dbac3c5c988b
Dima <yadima2001@gmail.com>
2021-11-28 14:34:14 +0200

    starter

---
+++ Module2HW6/Starter.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Module2HW6.Provider;
+using Module2HW6.Provider.Abstractions;
+using Module2HW6.Services;
+using Module2HW6.Services.Abstractions;
+
+namespace Module2HW6
+{
+    public class Starter
+    {
+        public void Run()
+        {
+            var serviceProvider = new ServiceCollection()
+                .AddTransient<IApplianceProvider, ApplianceProvider>()
+                .AddTransient<IApplianceService, ApplianceService>()
+                .AddTransient<App>()
+                .BuildServiceProvider();
+            var app = serviceProvider.GetService<App>();
+            app?.Run();
+        }
+    }
+}
